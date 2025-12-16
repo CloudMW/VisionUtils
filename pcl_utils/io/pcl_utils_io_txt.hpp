90f045b347adf5468e8901ad1b098123134e5fbf
CloudMW <mfy.2000@outlook.com>
2025-12-16 13:34:48 +0800

    add pcl_utils_vis_container.hpp and demo

--- pcl_utils/io/pcl_utils_io_txt.hpp
+++ pcl_utils/io/pcl_utils_io_txt.hpp
@@ -8,7 +8,7 @@
 #include <pcl/point_types.h>
 #include <spdlog/spdlog.h>
 #include <iosfwd>
-#include <direct.h>
+//#include <direct.h>
 #include <fstream>
 namespace pcl_utils
 {
@@ -135,6 +135,56 @@ namespace pcl_utils
 
                 // 解析 x y z（忽略多余空格/制表符）
                 if (!(ss >> p.x >> p.y >> p.z >> p.intensity))
+                {
+                    spdlog::warn("line {} format error ,skipp:{}", line_num, line);
+                    continue;
+                }
+
+                // 可选：忽略行内多余数据（如误加的颜色/强度值）
+                // float dummy;
+                // if (ss >> dummy) {
+                //     std::cerr << "[WARNING] 第 " << line_num << " 行数据冗余，仅读取 x y z" << std::endl;
+                // }
+                cloud->push_back(p);
+            }
+
+            file.close();
+
+            // 验证读取结果
+            if (cloud->empty())
+            {
+                spdlog::error("data is empty, file:{} ", file_path);
+                return false;
+            }
+            spdlog::info("open data success!file :{},point size:{}", file_path, cloud->size());
+            return true;
+        }
+
+        bool readTXTToPCLXYZ(const std::string& file_path, const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud)
+        {
+            // 初始化点云
+            cloud->clear();
+
+            // 打开文件
+            std::ifstream file(file_path);
+            if (!file.is_open())
+            {
+                spdlog::error("can't open:{}", file_path);
+                return false;
+            }
+
+            std::string line;
+            int line_num = 0;
+
+            // 逐行解析
+            while (std::getline(file, line))
+            {
+                line_num++;
+                std::stringstream ss(line);
+                pcl::PointXYZ p;
+
+                // 解析 x y z（忽略多余空格/制表符）
+                if (!(ss >> p.x >> p.y >> p.z))
                 {
                     spdlog::warn("line {} format error ,skipp:{}", line_num, line);
                     continue;
