7d09e0d4a3ae13ba23b42128a8a86a896c039a15
CloudMW <mfy.2000@outlook.com>
2025-12-23 08:56:21 +0800

    seg error test

--- demo/pcl_utils/segmentation/main.cpp
+++ demo/pcl_utils/segmentation/main.cpp
@@ -6,6 +6,7 @@
 #include "pcl_utils/visualization/pcl_utils_visualization.hpp"
 #include "pcl_utils/io/pcl_utils_io_txt.hpp"
 #include "pcl_utils/segmentation/pcl_utils_clusters.hpp"
+#include "segmentation/pcl_uitls_sac_segment.hpp"
 #include "spdlog/spdlog.h"
 
 typedef  pcl::PointXYZI PointT;
@@ -22,6 +23,7 @@ int main() {
 
         // clusters
         {
+            /*
             {
                 //euclideanClustering
                 std::vector<pcl::PointCloud<PointT>::Ptr> clusters;
@@ -29,8 +31,34 @@ int main() {
 
                 pcl_utils::segmentation::euclideanClustering<PointT>(cloud_2, clusters, cloudRGB,10 ,10 ,100000);
                 spdlog::info("clusters size: {}",clusters.size());
-                pcl_utils::visualization::showPointCloud<pcl::PointXYZRGB>("clusters", cloudRGB);
+               // pcl_utils::visualization::showPointCloud<pcl::PointXYZRGB>("clusters", cloudRGB);
             }
+            */
+        }
+
+
+        //seg
+        {
+            /*pcl_utils::sac_segmentation::fitAndSegment<PointT>(cloud_1,
+                                                               pcl::SACMODEL_PLANE,
+                                                               10,
+                                                               10000,
+                                                               plane_cloud,
+                                                               remaining_cloud,
+                                                               planeCoefficients
+            );*/
+            auto container_cloud = std::make_shared<pcl::PointCloud<PointT>>();
+            auto container_coefficients = std::make_shared<pcl::ModelCoefficients>();
+            pcl_utils::sac_segmentation::fitAndSegment<PointT>(cloud_2,
+                                                               pcl::SACMODEL_NORMAL_PLANE,
+                                                               20.0,
+                                                               1000,
+                                                               container_cloud,
+                                                               nullptr,
+                                                               container_coefficients,
+                                                               200.0,
+                                                               Eigen::Vector3f(1,0,0),30.0
+            );
         }
 
     }
